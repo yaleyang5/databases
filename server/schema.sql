b5380c16e8740c76f1a4e06b69a77dcbdd294071
Tucker Williams <thrack.gorsh@gmail.com>
2023-03-01 16:38:25 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,37 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(20)
+);
+
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  text VARCHAR(144),
+  user_id INT,
+  room_id INT,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id),
+  FOREIGN KEY (room_id)
+    REFERENCES rooms(id)
+);
+
+CREATE TABLE friends (
+  user1_id INT,
+  user2_id INT,
+  FOREIGN KEY (user1_id)
+    REFERENCES users(id),
+  FOREIGN KEY (user2_id)
+    REFERENCES users(id)
+);
 
 
 
